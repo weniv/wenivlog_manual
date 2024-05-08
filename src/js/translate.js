5e0b993cd95a464cbdbd459c28e48e5448111498
baekg110 <baekg.dev@gmail.com>
2024-05-08 17:01:41 +0900

    :memo: Edit: 스폰서 디자인 수정

--- src/js/translate.js
+++ src/js/translate.js
@@ -17,13 +17,17 @@ const translations = {
     'target-main-txt':
       "<span class='light'> GitHub Pages를 이용하여 <br /></span>나만의 정적 블로그를 <br />쉽게 구현할 수 있어요",
     'target-list-1': '블로그 글도 쓰면서 깃헙 잔디도 심고 싶으신 분',
-    'target-list-2': '지킬(Jekyll)을 이용해 블로그를 만드는데 어려움을 느끼셨던 분',
+    'target-list-2':
+      '지킬(Jekyll)을 이용해 블로그를 만드는데 어려움을 느끼셨던 분',
     'target-list-3': '나만의 개발 블로그를 만들어보고 싶었던 분',
-    'target-list-4': '매일 공부한 것을 정리(TIL, Today I Learned)하고 싶으신 분',
+    'target-list-4':
+      '매일 공부한 것을 정리(TIL, Today I Learned)하고 싶으신 분',
     'target-list-5': '코드 하이라이팅이 가능한 블로그를 원하시는 분',
     'target-list-6': 'ipynb 파일을 블로그에 업로드하고 싶으신 분',
     'guide-main-txt': '사용 방법을 알려드릴게요',
     'guide-sub-txt': '"차근차근 따라하면 나만의 GitHub Blog 탄생!"',
+    'lecture-sub': '더 자세한 내용은 영상 강의에서 확인하세요!',
+    'lecture-btn': '강의 바로가기',
     'guide-item-1': '제공된 레포지토리를 fork 합니다.',
     'guide-link-btn': '링크 바로가기',
     'guide-item-2': '내 레포지토리의 github pages를 활성화합니다.',
@@ -57,6 +61,14 @@ const translations = {
     'industry-value': '정보통신업',
     address: '주소:',
     'address-value': '제주 제주시 첨단로 330 세미양빌딩 A동 1층 106호',
+    'sponsor-cont': '스폰서 등록',
+    'sponsor-main': '위니브의 교육 활동을 지원하세요',
+    'sponsor-sub':
+      'GitHub 스폰서를 등록하고 오픈소스 프로젝트, 책 집필 등 위니브의 교육 활동을 지원하고 기여자로 참여하세요!',
+    'sponsor-title': '위니브 깃허브 스폰서 정보',
+    'sponsor-corp': '주식회사 위니브',
+    'sponsor-descript':
+      '제주에 위치한 ICT 교육 콘텐츠 기업으로 청년들을 위한 다양한 교육 사업을 진행하고 있습니다.',
   },
   en: {
     manual: 'Manual',
@@ -73,15 +85,21 @@ const translations = {
     'target-sub-txt':
       'Did you have difficulty creating a GitHub Blog?<br />Try using wenivlog!',
     'target-main-txt':
-    "<span class='light'>using GitHub Pages <br /></span> Make your own static blog",
-    "target-list-1": "Those who want to write blog posts and grow their GitHub grass at the same time",
-    "target-list-2": "Those who experienced difficulties creating a blog using Jekyll",
-    "target-list-3": "Those who wanted to create their own development blog",
-    "target-list-4": "Those who want to organize what they study every day (TIL, Today I Learned)",
-    "target-list-5": "Those who want a blog with code highlighting capabilities",
-    "target-list-6": "Those who want to upload ipynb files to their blog",
+      "<span class='light'>using GitHub Pages <br /></span> Make your own static blog",
+    'target-list-1':
+      'Those who want to write blog posts and grow their GitHub grass at the same time',
+    'target-list-2':
+      'Those who experienced difficulties creating a blog using Jekyll',
+    'target-list-3': 'Those who wanted to create their own development blog',
+    'target-list-4':
+      'Those who want to organize what they study every day (TIL, Today I Learned)',
+    'target-list-5':
+      'Those who want a blog with code highlighting capabilities',
+    'target-list-6': 'Those who want to upload ipynb files to their blog',
     'guide-main-txt': 'Let us guide you through',
     'guide-sub-txt': '"Follow step by step to create your own GitHub Blog!"',
+    'lecture-sub': 'Check out the video lecture for more details!',
+    'lecture-btn': 'Go to Lecture',
     'guide-item-1': 'Fork the provided repository.',
     'guide-link-btn': 'Go to Link',
     'guide-item-2': 'Enable GitHub Pages for your repository.',
@@ -114,6 +132,14 @@ const translations = {
     'industry-value': 'ICT',
     address: 'Address',
     'address-value': '1F, A-106, 330, Cheomdan-ro, Jeju-si, Jeju-do',
+    'sponsor-cont': 'Sponsor Registration',
+    'sponsor-main': 'Support Weniv’s Educational Activities',
+    'sponsor-sub':
+      'Register as a GitHub Sponsor and support Weniv’s educational activities such as open source projects and book writing, and participate as a contributor!',
+    'sponsor-title': 'Weniv GitHub Sponsor Information',
+    'sponsor-corp': 'Weniv Corporation',
+    'sponsor-descript':
+      'An ICT education content company located in Jeju, conducting various education programs for young people.',
   },
 };
 
